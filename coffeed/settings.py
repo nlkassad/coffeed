a06c51a1481d2e4a03ee48b5c2f0f3569ff5f4a5
nlkassad <nlkassad@hotmail.com>
2016-06-27 21:29:20 -0400

    Updating settings and wsgi

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -82,10 +82,15 @@ DATABASES = {
     }
 }
 
-# Update database configuration with $DATABASE_URL.
+# Parse database configuration from $DATABASE_URL
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 
 # Internationalization
